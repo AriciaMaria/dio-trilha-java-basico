11b2cdf515369cc1df3ade1f4148cff20190bad9
AriciaMaria <ariciamsc@gmail.com>
2025-05-02 13:01:33 -0300

    ResultadoEscolar

--- Controle-Fluxo/src/CaixaEletronico.java
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -1,10 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double ValorSolicitado = 26.0;
+        double ValorSolicitado = 22.0;
 
-        if (ValorSolicitado < saldo)
+        if (ValorSolicitado < saldo) {
            saldo = saldo - ValorSolicitado;
+           System.out.println("Novo saldo: " + saldo);
+
+    }else
+           System.out.println("Saldo insuficiente");
 
            System.out.println(saldo);
     }
